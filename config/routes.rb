a1d2d0cb265572a52256dbadea17b319693695d6
Wp1987 <perlichw@oregonstate.edu>
2017-01-31 21:50:01 -0800

    link from login page to create new user page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   root controller: 'user_sessions', action: 'new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/users/new", to: "users#new", as: "createuser"
+
 end

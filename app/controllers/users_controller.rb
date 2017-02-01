5634ec6e3c352bf64f894c1aaa98f1cc5308c4f8
Wp1987 <perlichw@oregonstate.edu>
2017-01-31 20:04:17 -0800

    form to create new users is accepting values but values not finding their way to DB yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(params[:user])    # Not the final implementation!
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
+ def create
+    @user = User.new(user_params)
+    # ...
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :salt, :encrypted_password)
   end
 end
 

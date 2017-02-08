d070ce0bf93c0feea3e28cc27fcda4c3672ae334
Wp1987 <perlichw@oregonstate.edu>
2017-02-07 21:14:40 -0800

    login, award model, and added role to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to '/'
+      flash[:notice] = 'User saved successfully'
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208040121) do
+ActiveRecord::Schema.define(version: 20170208050757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170208040121) do
     t.string   "perishable_token"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "role"
   end
 
   add_foreign_key "awards", "users"

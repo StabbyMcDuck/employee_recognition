545a580585d0b8158ba0da7c2df9225958c350f7
Wp1987 <perlichw@oregonstate.edu>
2017-02-20 23:02:03 -0800

    Password reset working! Created gmail account for mailer see setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "asciicasts.com",
-  :user_name            => "wperlich",
-  :password             => "secret",
+  :user_name            => "employeerecognitioncenter",
+  :password             => "asdf12345",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }

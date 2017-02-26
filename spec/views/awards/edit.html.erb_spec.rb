8f306e5ceb318d8b9fe84161a6bfbdc0701c7943
Wp1987 <perlichw@oregonstate.edu>
2017-02-26 14:03:19 -0800

    more test fix

--- spec/views/awards/edit.html.erb_spec.rb
+++ spec/views/awards/edit.html.erb_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "awards/edit", type: :view do
   before(:each) do
-  	view.stub(:current_user) { User.new # return a user }
+  	view.stub(:current_user) { User.new # return a user }#
     @award = assign(:award, FactoryGirl.create(:user_award))
   end
 
   it "renders the edit award form" do
     render
-
     assert_select "form[action=?][method=?]", award_path(@award), "post" do
     end
   end
